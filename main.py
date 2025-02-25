7dd98a9a8675bf0fa40692b14b070486aa7b9cf4
郭鹏 <gp@pp052.top>
2025-02-25 14:20:22 +0800

    初版测试

--- main.py
+++ main.py
@@ -16,19 +16,50 @@ user_dict = {
 
 @register("vinda", "pp", "自用vinda助手", "1.0.0", "https://github.com/a490077/astrbot_plugin_vd")
 class VindaPlugin(Star):
+    """vinda助手, 需先绑定工号才能使用"""
+
     def __init__(self, context: Context, config: dict):
         super().__init__(context)
         self.config = config
         self.vinda = Vinda(self.config)
         print(self.config)
 
+    @filter.command("菜单")
+    async def 菜单(self, event: AstrMessageEvent):
+        """获取今日菜单"""
+        reply_message = self.vinda.菜单()
+        yield event.plain_result(reply_message)
+
+    @filter.command("稽查")
+    async def 稽查(self, event: AstrMessageEvent):
+        """查询今日订餐情况"""
+        reply_message = self.vinda.稽查()
+        yield event.plain_result(reply_message)
+
     @filter.command("订餐")
-    async def helloworld(self, event: AstrMessageEvent):
+    async def 订餐(self, event: AstrMessageEvent):
         """订餐命令"""
         user_name = event.get_sender_name()
-        message_str = event.message_str  # 用户发的纯文本消息字符串
-        message_chain = event.get_messages()  # 用户所发的消息的消息链
-        logger.info(message_chain)
-
+        logger.info(self.context.get_config())
         reply_message = self.vinda.do_order(user_dict.get(user_name)) if user_dict.get(user_name) else "你还不是VIP"
         yield event.plain_result(f"@{user_name} {reply_message}")
+
+    @filter.command("销餐")
+    async def 销餐(self, event: AstrMessageEvent):
+        """销餐命令"""
+        user_name = event.get_sender_name()
+        reply_message = self.vinda.pin_meal(user_dict.get(user_name)) if user_dict.get(user_name) else "你还不是VIP"
+        yield event.plain_result(f"@{user_name} {reply_message}")
+
+    @filter.command("二维码")
+    async def 菜单(self, event: AstrMessageEvent):
+        """获取今日菜单"""
+        user_name = event.get_sender_name()
+        reply_message = self.vinda.get_qr_code_data(user_dict.get(user_name)) if user_dict.get(user_name) else "你还不是VIP"
+        yield event.plain_result(reply_message)
+
+    @filter.command("查询")
+    async def 查询(self, event: AstrMessageEvent, name: str = None):
+        """根据名称查询员工信息"""
+        reply_message = self.vinda.查询(name)
+        yield event.plain_result(reply_message)
