8aafd3ad96a62a0054fb4c8d82109272bf836a72
郭鹏 <gp@pp052.top>
2025-02-25 15:30:14 +0800

    初版测试

--- main.py
+++ main.py
@@ -67,8 +67,30 @@ class VindaPlugin(Star):
         reply_message = self.vinda.查询(name)
         yield event.plain_result(reply_message)
 
-    @filter.llm_tool(name="get_menu")  # 如果 name 不填，将使用函数名
+    @filter.llm_tool()
     async def get_menu(self, event: AstrMessageEvent):
         """获取今天的菜单, 不用任何参数, 当用户需要查看菜单时, 可以使用这个函数, 例如用户想知道今天吃什么的时候"""
         reply_message = self.vinda.菜单()
+        yield event.plain_result(reply_message)
+
+    @filter.llm_tool()
+    async def do_order(self, event: AstrMessageEvent):
+        """订餐函数, 用户想要报餐或订餐时调用, 不用任何参数"""
+        user_name = event.get_sender_name()
+        reply_message = self.vinda.do_order(user_dict.get(user_name)) if user_dict.get(user_name) else "你还不是VIP"
+        yield event.plain_result(f"@{user_name} {reply_message}")
+
+    @filter.llm_tool()
+    async def pin_meal(self, event: AstrMessageEvent):
+        """取消订餐函数, 用户想要取消报餐或取消订餐时调用, 不用任何参数"""
+        user_name = event.get_sender_name()
+        reply_message = self.vinda.pin_meal(user_dict.get(user_name)) if user_dict.get(user_name) else "你还不是VIP"
+        yield event.plain_result(f"@{user_name} {reply_message}")
+
+    @filter.llm_tool()
+    async def looklook(self, event: AstrMessageEvent):
+        """查看今天的订餐情况, 当用户想要查询今天有哪些人订餐和没有订餐时调用,
+        例如用户想要看看今天的订餐情况, 或者用户说提到 稽查 等词语时调用
+        不用任何参数"""
+        reply_message = self.vinda.稽查(user_dict)
         yield event.plain_result(reply_message)
