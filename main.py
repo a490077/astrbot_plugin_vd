a70f3b2640a63baef6554fc1f441841deb3f62f4
郭鹏 <gp@pp052.top>
2025-05-28 21:17:18 +0800

    初版测试

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ class VindaPlugin(Star):
             for key, value in result.items():
                 if re.match(pattern, key):
                     元宝 += value.get("元宝", 0)
-                    result_text += f"区服: {key}	💰: {value.get('元宝',0):>7}\t{'🏅' if value.get('侠名') else ''}\n"
+                    result_text += f"区服: {key:<10} 💰: {value.get('元宝', 0):>7,} {('🏅' if value.get('侠名') else '')}\n"
 
             result_text += f"合计💰: {元宝}"
             # yield event.plain_result(result_text)
